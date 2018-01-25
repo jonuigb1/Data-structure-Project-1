25bf1412a3953c168a62e1caa3f2817a3f3dd54c
jonuigb1 <jonuigb1@my.westga.edu>
2018-01-25 15:18:03 -0500

    Initial Commit

---
+++ src/model/Directory.java
@@ -0,0 +1,40 @@
+package model;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.swing.JFileChooser;
+
+public class Directory {
+private static ArrayList<String> list;
+	
+	private static void addFiles(File dir) {
+		if(dir.isDirectory()) {
+			for(File file : dir.listFiles()) {
+				list.add(file.getAbsolutePath());
+				if(file.isDirectory()) {
+					addFiles(file);
+				}
+			}
+		}
+	}
+	public static void main(String[] args) {
+		
+		list = new ArrayList<String>();
+		JFileChooser chooser = new JFileChooser();
+		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		chooser.setFileSelection
+		chooser.setAcceptAllFileFilterUsed(false);
+		int value = chooser.showOpenDialog(null);
+		if(value != JFileChooser.APPROVE_OPTION) {
+			return;
+		}
+		addFiles(chooser.getSelectedFile());
+		for(String name : list) {
+			System.out.println(name);
+		}
+
+	}
+
+
+}
