68fda0559bcdb00ebef48d485d24ccbe7aa3babf
jonuigb1 <jonuigb1@my.westga.edu>
2018-01-25 15:23:07 -0500

    Created a view and view_model

--- src/model/Directory.java
+++ src/model/Directory.java
@@ -23,7 +23,8 @@ private static ArrayList<String> list;
 		list = new ArrayList<String>();
 		JFileChooser chooser = new JFileChooser();
 		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-		chooser.setFileSelection
+		chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
 		chooser.setAcceptAllFileFilterUsed(false);
 		int value = chooser.showOpenDialog(null);
 		if(value != JFileChooser.APPROVE_OPTION) {
@@ -34,6 +35,9 @@ private static ArrayList<String> list;
 			System.out.println(name);
 		}
 
+	}
+	public void search() {
+		//TODO
 	}
 
 
---
+++ src/view_model/MainWindow.java
@@ -0,0 +1,5 @@
+package view_model;
+
+public class MainWindow {
+
+}
