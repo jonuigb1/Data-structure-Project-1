e4c1605788bdaa79e17c553d45979c582340443c
jonuigb1 <jonuigb1@my.westga.edu>
2018-01-25 19:47:15 -0500

    added methods for MainWindow

--- src/view_model/MainWindow.java
+++ src/view_model/MainWindow.java
@@ -1,8 +1,11 @@
 package view_model;
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 
@@ -13,6 +16,9 @@ public class MainWindow {
 
     @FXML
     private URL location;
+
+    @FXML
+    private Button RootDirectory;
 
     @FXML
     private TextField searchText;
@@ -33,8 +39,20 @@ public class MainWindow {
     private RadioButton FullPath;
 
     @FXML
+    private ListView<?> outputTextArea;
+
+    @FXML
+    void DisplayRootDirectory(ActionEvent event) {
+    	File[] rootDirectory = File.listRoots();
+    	for( int i = 0; i < rootDirectory.length; i++) {
+    		System.out.println(rootDirectory[i].toString());
+    	}
+    	 
+
+    }
+
+    @FXML
     void getDirectories(ActionEvent event) {
-    	
 
     }
 
@@ -60,15 +78,14 @@ public class MainWindow {
 
     @FXML
     void initialize() {
+        assert RootDirectory != null : "fx:id=\"RootDirectory\" was not injected: check your FXML file 'MainWindow.fxml'.";
         assert searchText != null : "fx:id=\"searchText\" was not injected: check your FXML file 'MainWindow.fxml'.";
         assert SelectAllFIlesAndDirectories != null : "fx:id=\"SelectAllFIlesAndDirectories\" was not injected: check your FXML file 'MainWindow.fxml'.";
         assert SelectDirectories != null : "fx:id=\"SelectDirectories\" was not injected: check your FXML file 'MainWindow.fxml'.";
         assert SelectFiles != null : "fx:id=\"SelectFiles\" was not injected: check your FXML file 'MainWindow.fxml'.";
         assert NameOnly != null : "fx:id=\"NameOnly\" was not injected: check your FXML file 'MainWindow.fxml'.";
         assert FullPath != null : "fx:id=\"FullPath\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert outputTextArea != null : "fx:id=\"outputTextArea\" was not injected: check your FXML file 'MainWindow.fxml'.";
 
     }
 }
-
-
-
